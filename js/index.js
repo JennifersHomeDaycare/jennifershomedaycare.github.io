55cc672341709539245d9c353e2b67a3c273384e
25gmax <55412802+25gmax@users.noreply.github.com>
2019-12-31 13:41:51 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 function includeHTML() { //Thanks w3!
   var z, i, elmnt, file, xhttp;
   /*loop through a collection of all HTML elements:*/
@@ -26,3 +25,36 @@ function includeHTML() { //Thanks w3!
     }
   }
 };
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
